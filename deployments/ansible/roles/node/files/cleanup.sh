414961c34bea6ceb7183bccc5c84565de60de537
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2024-01-31 23:31:19 +0100

    commit

---
+++ deployments/ansible/roles/node/files/cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker compose -f ~/docker-compose-`hostname`.yml down
+docker system prune -a -f
+sudo virsh destroy test; sudo virsh undefine test || true
+for i in $(seq 0 10); do
+	sudo virsh destroy test$i
+	sudo virsh undefine test$i
+done
+docker ps -aq | xargs docker stop | xargs docker rm || true
