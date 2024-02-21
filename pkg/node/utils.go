73be36bbb99accadb815e9fbbd1a75924f4c1230
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2024-02-21 01:34:48 +0100

    commit

--- pkg/node/utils.go
+++ pkg/node/utils.go
@@ -120,7 +120,7 @@ func CreateSSHKey() error {
 // AddSSHPublicKey adds the ssh public key to image.
 func AddSSHPublicKey(image string) error {
 	logrus.Println("Adding ssh public key to", image)
-	cmd := exec.Command("LIBGUESTFS_BACKEND=direct virt-copy-in -a " + image + "/root/.ssh/authorized_keys " + " /root/.ssh")
+	cmd := exec.Command("LIBGUESTFS_BACKEND=direct virt-copy-in -a " + image + " /root/.ssh/authorized_keys " + " /root/.ssh")
 	res, err := cmd.CombinedOutput()
 	if err != nil {
 		logrus.Println(string(res))
