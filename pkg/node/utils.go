d3fc86fc1a55eb43afd3377916db1c9e4327cd68
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2024-02-23 23:58:20 +0100

    commit

--- pkg/node/utils.go
+++ pkg/node/utils.go
@@ -119,7 +119,8 @@ func CreateSSHKey() error {
 // AddSSHPublicKey adds the ssh public key to image.
 func AddSSHPublicKey(image string) error {
 	logrus.Println("Adding ssh public key to", image)
-	mkdirCmd := exec.Command("/usr/bin/virt-customize", "--no-selinux-relabel", "-a", image, "--mkdir", "/root/.ssh", "--root-password", "password:root", "--password", "password:password", "--network", "--ssh-inject", "root:file:/root/.ssh/id_rsa.pub")
+	cmdSlice := []string{"--no-selinux-relabel", "-a", image, "--mkdir", "/root/.ssh", "--root-password", "password:root", "--password", "password:password", "--network", "--ssh-inject", "root:file:/root/.ssh/id_rsa.pub"}
+	mkdirCmd := exec.Command("virt-customize", cmdSlice...)
 	_, err := mkdirCmd.CombinedOutput()
 	if err != nil {
 		logrus.Println(err)
