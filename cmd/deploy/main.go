60b3d80073294e26e93a7344e76ea8485b186a9c
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2024-03-05 00:56:25 +0100

    commit

--- cmd/deploy/main.go
+++ cmd/deploy/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "os"
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+
+var rootCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "deploy a shitty cloud",
+	Long:  `deploy a shitty cloud`,
+	Run: func(cmd *cobra.Command, args []string) {
+		
+
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
 func main() {
-	return
+	Execute()
 }
